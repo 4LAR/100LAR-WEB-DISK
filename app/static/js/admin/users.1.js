617fe543644c492f6b3a6704bf8dc87b0e60ea29
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2022-09-27 22:54:38 +0300

    вывод информации о пользователе

--- app/static/js/admin/users.1.js
+++ app/static/js/admin/users.1.js
@@ -1,5 +1,17 @@
 var users_JSON = {};
 
+function open_close_user(name) {
+  if (document.getElementById('users_' + name).style.height == '30px') {
+    document.getElementById('users_' + name).style.height = '';
+    document.getElementById('users_triangle_' + name).style.transform = 'rotate(0deg)';
+    openModal('users_info_' + name);
+  } else {
+    document.getElementById('users_' + name).style.height = '30px';
+    document.getElementById('users_triangle_' + name).style.transform = 'rotate(90deg)';
+    closeModal('users_info_' + name);
+  }
+}
+
 function get_users() {
   var xhr = new XMLHttpRequest();
   xhr.open('POST', `/get_users`);
@@ -8,15 +20,49 @@ function get_users() {
     if (xhr.status === 200) {
       users_JSON = JSON.parse(xhr.responseText.toString());
       console.log(users_JSON);
+
+      var i = Object.keys(users_JSON).length - 1;
       for (user in users_JSON) {
+        var path_list_str = '';
+        for (let i = 0; i < users_JSON[user]['path'].length; i++) {
+          if (users_JSON[user]['path'][i]['type'] == 'path') {
+            path_list_str += `
+              <br>
+              <p>type: ${users_JSON[user]['path'][i]['type']}</p><br>
+              <p>name: ${users_JSON[user]['path'][i]['name']}</p><br>
+              <p>path: ${users_JSON[user]['path'][i]['path']}</p><br>
+              <p>size: ${users_JSON[user]['path'][i]['size']}</p><br>
+            `
+          } else {
+            path_list_str += `
+              <br>
+              <p>type: ${users_JSON[user]['path'][i]['type']}</p><br>
+              <p>name: ${users_JSON[user]['path'][i]['name']}</p><br>
+            `
+          }
+
+        }
+        console.log(i);
         append_to_ul(
           'userlist',
           `
-            <div style="height: 30px;">
-              <img class="icon" width="20" height="20" src="static/img/user.svg">
-              <p>${user}</p>
+            <div id="users_${user}" style="height: 30px" class="block_select">
+              <div onclick="open_close_user('${user}')" style="cursor: pointer">
+                <img class="icon" width="20" height="20" src="static/img/user.svg">
+                <p>${user}</p>
+                <img align="right" id="users_triangle_${user}" style="margin: 5px 20px; transform: rotate(90deg)" class="icon" width="10" height="10" src="static/img/triangle.svg">
+              </div>
+              <div id="users_info_${user}" class="user_info" style="display: none">
+                <br>
+                <p>STATUS: ${users_JSON[user]['status']}</p><br>
+                <p>PASSWORD: ${users_JSON[user]['password']}</p><br>
+                <p>PANEL: ${users_JSON[user]['panel']}</p><br>
+
+                ${path_list_str}
+                <br>
+              </div>
             </div>
-            <hr class="main_page_hr">
+            ${(i-- != 0)? '<hr class="main_page_hr">': ''}
           `
         );
       }
