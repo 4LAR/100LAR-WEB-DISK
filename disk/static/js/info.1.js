7bdedae2e96a697bd27579b45815dae0502036cd
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2023-05-09 14:29:08 +0300

    небольшие поправки

--- disk/static/js/info.1.js
+++ disk/static/js/info.1.js
@@ -127,6 +127,8 @@ function close_dialogs() {
     close_change_login_dialog();
   } else if (image_fullscreen_bool) {
     image_fullscreen();
+  } else if (video_fullscreen) {
+    full_screen();
   } else return false;
   return true;
 }
