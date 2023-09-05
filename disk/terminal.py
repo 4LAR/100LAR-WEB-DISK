fb871bc6cfa1337ebacd8aa11e6fc135ec154748
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2023-09-05 10:10:42 +0300

    выделение виртуальной памяти терминалу

--- disk/terminal.py
+++ disk/terminal.py
@@ -3,6 +3,7 @@ import pty
 import os
 import signal
 import subprocess
+import resource
 import select
 import termios
 import struct
@@ -11,12 +12,16 @@ import shlex
 import sys
 
 class Terminal:
-    def __init__(self, cmd = ["bash"], cwd=None):
+    def __init__(self, cmd = ["bash"], cwd=None, memory_limit=None):
         self.fd = None
         self.child_pid = None
         self.process = None
         self.cmd = cmd
         self.cwd = cwd
+        self.memory_limit = memory_limit
+
+    def limit_virtual_memory(self):
+        resource.setrlimit(resource.RLIMIT_AS, (self.memory_limit, resource.RLIM_INFINITY))
 
     def set_winsize(self, row, col, xpix=0, ypix=0):
         if (self.fd):
@@ -36,7 +41,10 @@ class Terminal:
 
         (child_pid, fd) = pty.fork()
         if (child_pid == 0):
-            self.process = subprocess.run(self.cmd, cwd=self.cwd)
+            if self.memory_limit:
+                self.process = subprocess.run(self.cmd, cwd=self.cwd, preexec_fn=self.limit_virtual_memory)
+            else:
+                self.process = subprocess.run(self.cmd, cwd=self.cwd)
 
         else:
             self.fd = fd
